5fb927d764b0e2bcae0efea5b01bc87f54390aaa
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 18:54:50 +0530

    updates

---
+++ strinfinbuiltfunction.js
@@ -0,0 +1,32 @@
+var str= "welocome to javascript"
+console.log(str.length)
+
+var str="these are string inbuilt ficntions"
+console.log(str.indexOf('inbuilt'))
+
+var str="these are string inbuilt functions"
+console.log(str.slice(2,10))
+
+var str="these are string inbuilt function"
+console.log(str.substring(10))
+
+var str="these are string inbuilt fucntion"
+console.log(str.toUpperCase(str))
+
+var str="THESE ARE STRING INBUILT FUNCTION"
+console.log(str.toLowerCase(str))
+
+var fstr="these"
+var sstr="are string"
+var tstr="inbuilt"
+var pstr="fucntion"
+console.log(fstr.concat('', sstr).concat(tstr).concat(pstr));
+
+var str="these   are     string    inbuilt    function"
+ console.log(str.trim(str));
+
+ var str="these are string inbuilt fucntion"
+ console.log(str.charAt(18))
+
+ var str="these are string inbuilt fucntion"
+ console.log(str.split(''))
